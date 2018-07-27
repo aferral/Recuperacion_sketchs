3f1510dda3dbb56525727971dbdc678d22ae32e6
Andres <andreslago5@hotmail.com>
2018-07-27 14:24:48 -0400

    Agregada nueva arquitectura

--- run.py
+++ run.py
@@ -14,10 +14,13 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from subprocess import call 
 import zipfile
+from tensorflow.contrib.layers import fully_connected, conv2d,max_pool2d
+from tensorflow.contrib.layers import xavier_initializer, xavier_initializer_conv2d
+from tensorflow.layers import batch_normalization
 
 
 # Leer tf record en dataset y comprobar que imagenes sigan bien (y que esten bien mezcladas)
-
+from residual_arch import arch_skNetI_residual
 
 train_tf_record = "/home/aferral/Escritorio/temp/train_2018_07_26__21_50_54.tfrecord"
 
@@ -44,9 +47,6 @@ def get_dataset(tf_records,epochs,batch_size,n_classes,shuffle_buffer=500):
  
   return dataset
 
-from tensorflow.contrib.layers import fully_connected, conv2d,max_pool2d
-from tensorflow.contrib.layers import xavier_initializer, xavier_initializer_conv2d
-from tensorflow.layers import batch_normalization
 
 # crear dataset train, test
 def arch_skNetI(bx,by):
@@ -113,129 +113,103 @@ def arch_skNetI(bx,by):
 
 
 
-
-bs = 200
-lr=0.001
-dataset=get_dataset(train_tf_record,1,bs,100,shuffle_buffer=100000)
-
-
-
-
-# Armar arquitectura
-
-iterator = tf.data.Iterator.from_structure(dataset.output_types,dataset.output_shapes)
-
-bx,by = iterator.get_next()
-logits = arch_skNetI(bx,by)
-
-pred = tf.nn.softmax(logits, name='prediction')
-
-loss = tf.losses.sparse_softmax_cross_entropy(by, logits)
-train_step = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
-
-# get accuracy
-prediction = tf.argmax(logits, 1)
-equality = tf.equal(prediction, by)
-accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
-              
-
-def zip_folder(f_path,out_path_zip):
-
-    fantasy_zip = zipfile.ZipFile(out_path_zip, 'w')
-
-    for folder, subfolders, files in os.walk(f_path):
-        for file in files:
-            fantasy_zip.write(os.path.join(folder, file),
-                              os.path.relpath(os.path.join(folder, file)),
-                              compress_type=zipfile.ZIP_DEFLATED)
-    fantasy_zip.close()
-    pass
-  
-
-def save_model(saver,sess,save_name,upload=True):
-  now = datetime.now().strftime("%Y_%m_%d__%H:%M:%S")
-  path_model_checkpoint = os.path.join('saved_models',save_name,now)
-  print("Saving model at {0}".format(path_model_checkpoint))
-  os.makedirs(path_model_checkpoint,exist_ok=True)
-  path_checkpoint = os.path.join(path_model_checkpoint,'saved_model')
-  saver.save(sess, path_checkpoint)
-  
-  # upload to drive
-  if upload:
-    zip_name = 'ziped_{0}_{1}.zip'.format(save_name,now)
-    out_p =os.path.join('saved_models',zip_name)
-    zip_folder(path_model_checkpoint,out_p)
-    save_to_drive(out_p,zip_name,"tarea_deep")
-  
+if __name__ == "__main__":
+    bs = 200
+    lr=0.0001
+    epochs=20
+    dataset=get_dataset(train_tf_record,epochs,bs,100,shuffle_buffer=100000)
+    architecture = arch_skNetI_residual
 
 
+    # Armar arquitectura
 
-with tf.Session() as sess:
+    iterator = tf.data.Iterator.from_structure(dataset.output_types,dataset.output_shapes)
 
-  init_op = tf.global_variables_initializer()
-  sess.run(init_op)
-  
-  sess.run(iterator.make_initializer(dataset))
+    bx,by = iterator.get_next()
+    logits = architecture(bx,by)
 
-  
-  saver = tf.train.Saver()
+    pred = tf.nn.softmax(logits, name='prediction')
 
+    loss = tf.losses.sparse_softmax_cross_entropy(by, logits)
+    train_step = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
 
-  i = 0
-
-  # batch dataset 100.000 / 200 = 500 batchs
-  
-  while True:
-      try:
-          if i % 10 == 0:
-            now=datetime.now().strftime("%Y_%m_%d %H:%M:%S")
-            l, _, acc = sess.run([loss, train_step, accuracy])
-            print("t: {} , It: {}, loss_batch: {:.3f}, batch_accuracy: {:.2f}%".format(now,i, l, acc * 100))
-          else:
-            
-            sess.run(train_step)
+    # get accuracy
+    prediction = tf.argmax(logits, 1)
+    equality = tf.equal(prediction, by)
+    accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
 
 
-          if i % 50 == 0:
-            save_model(saver,sess,'train_1',upload=False)
+    def zip_folder(f_path,out_path_zip):
 
-          i += 1
+        fantasy_zip = zipfile.ZipFile(out_path_zip, 'w')
 
-      except tf.errors.OutOfRangeError:
-          print('break at {0}'.format(i))
-          break
+        for folder, subfolders, files in os.walk(f_path):
+            for file in files:
+                fantasy_zip.write(os.path.join(folder, file),
+                                  os.path.relpath(os.path.join(folder, file)),
+                                  compress_type=zipfile.ZIP_DEFLATED)
+        fantasy_zip.close()
+        pass
 
 
-  # todo TEST SET EVAL
+    def save_model(saver,sess,save_name,upload=True):
+      now = datetime.now().strftime("%Y_%m_%d__%H:%M:%S")
+      path_model_checkpoint = os.path.join('saved_models',save_name,now)
+      print("Saving model at {0}".format(path_model_checkpoint))
+      os.makedirs(path_model_checkpoint,exist_ok=True)
+      path_checkpoint = os.path.join(path_model_checkpoint,'saved_model')
+      saver.save(sess, path_checkpoint)
 
-
-
-  # Save model
-  save_model(saver,sess,'train_1',upload=False)
-
-  
- 
-#   print(a[0]) # Esto revisa que tan ordenadas estan las clases
+      # upload to drive
+      if upload:
+        zip_name = 'ziped_{0}_{1}.zip'.format(save_name,now)
+        out_p =os.path.join('saved_models',zip_name)
+        zip_folder(path_model_checkpoint,out_p)
+        save_to_drive(out_p,zip_name,"tarea_deep")
 
 
 
 
-# arquitecgura en funcion entrenar - checkpoints
-# opcion partir de check point
+    with tf.Session() as sess:
 
-# Usar loop de train, test.
+      init_op = tf.global_variables_initializer()
+      sess.run(init_op)
 
-# evaluar accuracy sklearn.
-# crear reporte csv o algo asi.
+      sess.run(iterator.make_initializer(dataset))
 
-# paso 2 activaciones.
-# cargar modelo 
-# cargar dataset a transformar. (test)
-# pasar nombre capas a extraer
-# guardar activaciones???
-# loop search 1 vs otros.
-# precomputar distancias en vectores.
-# conseguir ranking.
-# calcular map
 
+      saver = tf.train.Saver()
+
+
+      i = 0
+
+      # batch dataset 100.000 / 200 = 500 batchs
+
+      while True:
+          try:
+              if i % 10 == 0:
+                now=datetime.now().strftime("%Y_%m_%d %H:%M:%S")
+                l, _, acc = sess.run([loss, train_step, accuracy])
+                print("t: {} , It: {}, loss_batch: {:.3f}, batch_accuracy: {:.2f}%".format(now,i, l, acc * 100))
+              else:
+
+                sess.run(train_step)
+
+
+              if i % 50 == 0:
+                save_model(saver,sess,'train_1',upload=False)
+
+              i += 1
+
+          except tf.errors.OutOfRangeError:
+              print('break at {0}'.format(i))
+              break
+
+
+      # todo TEST SET EVAL
+
+
+
+      # Save model
+      save_model(saver,sess,'train_1',upload=False)
 
